74df3f87e2aa3554d51dd29eb175ad127d9f0d75
Skyler Parr <skylerparr@gmail.com>
2017-05-23 09:46:13 -0700

    added merchant id as part of the search params

--- lib/amazon_product_advertising_client/config.ex
+++ lib/amazon_product_advertising_client/config.ex
@@ -6,6 +6,7 @@ defmodule AmazonProductAdvertisingClient.Config do
   defstruct "AssociateTag": Application.get_env(:amazon_product_advertising_client, :associate_tag),
     "AWSAccessKeyId": Application.get_env(:amazon_product_advertising_client, :aws_access_key_id),
     "AWSSecret": Application.get_env(:amazon_product_advertising_client, :aws_secret_access_key),
+    "MerchantId": "Amazon", 
     "Service": "AWSECommerceService",
     "Version": "2013-08-01"
 
